bae08b0d7c6f4a7978d64050629b45451e7daa1a
kjTP-IND <144932028+kjTP-IND@users.noreply.github.com>
2023-10-16 16:32:59 -0400

    Add files via upload

---
+++ Uninstall Bloatware.ps1
@@ -0,0 +1,7 @@
+Get-AppxPackage ClipChamp.Clipchamp | Remove-AppxPackage;
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage;
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage;
+Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage;
+Get-AppxPackage Microsoft.GamingApp | Remove-AppxPackage;
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage;
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
